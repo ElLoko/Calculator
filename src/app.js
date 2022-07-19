2227228e86d0fb8c1afe91539893424f67c349c0
Gabriel Souza <gabrielsouzapaula7608@gmail.com>
2022-07-18 22:22:21 -0300

    .

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
+//var elemento = document.querySelector("#textBox");
+//elemento.parentNode.removeChild(elemento);
+
+  
+function keyPressed(evt){
+    evt = evt || window.event;
+    var key = evt.keyCode || evt.which;
+    return String.fromCharCode(key); 
+}
+
+document.onkeypress = function(evt) {
+    var str = Number(keyPressed(evt));
+    // MAX 17
+    cslen = document.getElementById("screen").innerText
+    if(str == str && cslen.length < 18)
+    document.getElementById("screen").innerText += typeof str == number ? str : str
+};
+
 function addNumber(number) {
     alert(number)
 }
 function arith(arithm) {
     alert(arithm)
-} 
+}
+function warnScreen() {
+    alert("The maximum is 18 characters")
+}
